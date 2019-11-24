02e058c5b0c59a66006c727c49c4a5932450a2ae
Mihir Joshi <mihir67mj@gmail.com>
2019-11-24 12:34:28 +0530

    Add files via upload

---
+++ TestingTravis/src/main/java/Travis/App.java
@@ -0,0 +1,20 @@
+package Travis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static boolean whoAmI(String name)
+    {
+        if(name.equals("Mihir Joshi"))
+            return true;
+        else 
+            return false;
+    }
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
